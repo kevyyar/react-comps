1d1bb504b5be8a94f9bf026a662510f92948ada3
Kevin Barreto <barretokevin@hotmail.com>
2023-04-03 16:56:05 -0600

    add expand behavior when clicking heading

--- src/components/Accordion.js
+++ src/components/Accordion.js
@@ -1,9 +1,21 @@
+import { useState } from "react";
+
 function Accordion({ items }) {
-  const renderedItems = items.map((item) => {
+  const [expandedIndex, setExpandedIndex] = useState(0);
+
+  const onExpandItem = (nextIndex) => {
+    setExpandedIndex(nextIndex);
+  };
+
+  const renderedItems = items.map((item, index) => {
+    const isExpanded = index === expandedIndex;
+
+    const content = <div>{item.content}</div>;
+
     return (
       <div key={item.id}>
-        <div>{item.heading}</div>
-        <div>{item.content}</div>
+        <div onClick={() => onExpandItem(index)}>{item.heading}</div>
+        {isExpanded && content}
       </div>
     );
   });
